f775c9f14c80ceda4978273473c40deb44ae097b
Julio Alejandro Santos Corona <59350645+jualesac@users.noreply.github.com>
2022-10-09 01:43:50 -0500

    Proyecto

---
+++ delta/index/js/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var CONTROLLER = {
+    main: function (use) {
+        let ajax = use.ajax;
+        let render = use.render;
+        let session = use.session;
+        let notification = use.extension.notification;
+
+
+        __construct.call (this);
+
+        function __construct () {
+
+        }
+    }
+};
