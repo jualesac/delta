64caf221b04637cf9603a16954edb9a0644d7ac4
Julio Alejandro Santos Corona <59350645+jualesac@users.noreply.github.com>
2022-10-09 01:39:26 -0500

    Proyecto

---
+++ apps/app/app1/js/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+CONTROLLER.app = function (use) {
+    let ajax = use.ajax;
+    let render = use.render;
+    let session = use.session;
+    let notification = use.extension.notification;
+
+
+    __init.call (this);
+
+    function __init () {
+
+    }
+};
